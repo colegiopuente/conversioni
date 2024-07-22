a9adf323b88253044806330806103f7b235bcfdc
Luis Parrado <luisprmat@gmail.com>
2024-07-22 11:51:02 -0500

    Update configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/conversion/',
+  base: '/conversioni/',
   plugins: [vue()],
   resolve: {
     alias: {
